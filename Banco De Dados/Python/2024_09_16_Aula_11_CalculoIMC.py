2161176eb1446314d7c6fae7c57d3a0c00a01020
Victor Roma <66295991+RomaNFS21@users.noreply.github.com>
2024-09-16 20:49:20 -0300

    .

---
+++ Banco De Dados/Python/2024_09_16_Aula_11_CalculoIMC.py
@@ -0,0 +1,9 @@
+def calculoIMC () :
+    numero1 = 30
+    numero2 = 50
+    soma = (numero1 + numero2)
+    resultado = soma / 2
+    print(f"A soma dos dois numeros da {soma} e a divisão por 2 da {resultado}")
+
+calculoIMC()
+    
