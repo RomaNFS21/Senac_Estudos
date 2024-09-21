7f3be91284bb9ea12cbb550c0da24053356b7771
Victor Roma <66295991+RomaNFS21@users.noreply.github.com>
2024-09-20 21:14:24 -0300

    Continuação do projeto do Hackathon

---
+++ Programação/Exercicios/JavaScript/Hackathon_Era_Espacial.js
@@ -0,0 +1,67 @@
+/* function idadeEmAnosPlaneta (segundos, periodoOrbital, planeta){
+    return (idadeSegundos);
+     };
+     var periodoOrbital;
+     var idadePlaneta;
+     var planeta = {
+     mercurio :  0.2408467,
+     venus : 0.6151972669,
+     marte : 1.88081583169,
+     jupiter  : 11.862615,
+     saturno : 29.447498,
+     urano : 84.016846,
+     netuno : 164.79132
+     }
+     for (let planeta in periodoOrbital) {
+    periodoOrbital= periodoOrbital[planeta];
+    idadePlaneta = idadeEmAnosPlaneta(idadeSegundos, periodoOrbital);
+    console.log(idade in (planeta))
+    };
+    #fim
+    */
+     /*
+    function idadeEmAnosPlaneta(segundos, periodoOrbital) {
+      const segundosPorAno = 31557600; 
+      return segundos / (segundosPorAno * periodoOrbital);
+  }
+  
+  const idadeSegundos = 1000000000; 
+  const periodosOrbitais = {
+      mercurio: 0.2408467,
+      venus: 0.6151972669,
+      marte: 1.88081583169,
+      jupiter: 11.862615,
+      saturno: 29.447498,
+      urano: 84.016846,
+      netuno: 164.79132
+  };
+  
+  for (let planeta in periodosOrbitais) {
+      const periodoOrbital = periodosOrbitais[planeta];
+      const idadePlaneta = idadeEmAnosPlaneta(idadeSegundos, periodoOrbital);
+      console.log('Idade em ${planeta}: ${idadePlaneta.toFixed(2)} anos')
+  };
+  */
+
+  function idadeEmAnosPlaneta (segundos, periodoorbital, planeta){
+    return (idadeSegundos);
+};
+    var periodoObital;
+    var idadePlaneta;
+    var planeta = {
+    mercurio: 0.2408467,
+    venus: 0.6151972669,
+    marte: 1.88081583169,
+    jupiter: 11.862615,
+    saturno: 29.447498,
+    urano: 84.016846,
+    netuno: 164.79132
+    }
+    for (let planeta in periodoorbital) {
+    periodoorbital= periodoorbital [planeta];
+    idadePlaneta = idadeEmAnosPlaneta(idadeSegundos, periodoorbital);
+    console.log(idade in (planeta))
+    };
+
+    // Fim do codigo.
+  
