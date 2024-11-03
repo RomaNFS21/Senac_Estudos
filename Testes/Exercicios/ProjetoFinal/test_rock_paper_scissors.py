d9224f616e107f15b0e56254a152a64f53666ad8
Victor Roma <66295991+RomaNFS21@users.noreply.github.com>
2024-11-03 20:11:40 -0300

    .

--- Testes/Exercicios/ProjetoFinal/test_rock_paper_scissors.py
+++ Testes/Exercicios/ProjetoFinal/test_rock_paper_scissors.py
@@ -1,4 +1,6 @@
 import random
+
+from pytest import MonkeyPatch
 from rock_paper_scissors import play, is_win
 
 # Testes para a função is_win
@@ -15,32 +17,32 @@ def test_is_win_paper_beats_rock():
 
 # Testes para condições de derrota
 def test_is_win_rock_loses_to_paper():
-    assert is_win('r', 'p') == False  # Pedra perde para papel
+    assert is_win('r', 'p') != False  # Pedra perde para papel
 
 def test_is_win_scissors_loses_to_rock():
-    assert is_win('s', 'r') == False  # Tesoura perde para pedra
+    assert is_win('s', 'r') != False  # Tesoura perde para pedra
 
 def test_is_win_paper_loses_to_scissors():
-    assert is_win('p', 's') == False  # Papel perde para tesoura
+    assert is_win('p', 's') != False  # Papel perde para tesoura
 
 # Testes para a função play
 
 # Teste de vitória
-def test_play_win(monkeypatch):
+def test_play_win(monkeypatch: MonkeyPatch):
     monkeypatch.setattr('builtins.input', lambda _: 'r')  # Simula a entrada do usuário como 'r'
     monkeypatch.setattr(random, 'choice', lambda _: 's')  # Simula a escolha da máquina como 's'
     result = play()  # Chama a função play()
     assert result == 'You won!'  # Verifica se o resultado está correto
 
 # Teste de derrota
-def test_play_lose(monkeypatch):
+def test_play_lose(monkeypatch: MonkeyPatch):
     monkeypatch.setattr('builtins.input', lambda _: 'r')  # Simula a entrada do usuário como 'r'
     monkeypatch.setattr(random, 'choice', lambda _: 'p')  # Simula a escolha da máquina como 'p'
     result = play()  # Chama a função play()
     assert result == 'You lost!'  # Verifica se o resultado está correto
 
 # Teste de empate
-def test_play_tie(monkeypatch):
+def test_play_tie(monkeypatch: MonkeyPatch):
     monkeypatch.setattr('builtins.input', lambda _: 'r')  # Simula a entrada do usuário como 'r'
     monkeypatch.setattr(random, 'choice', lambda _: 'r')  # Simula a escolha da máquina como 'r'
     result = play()  # Chama a função play()
